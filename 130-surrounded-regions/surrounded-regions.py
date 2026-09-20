ec903a33f67860288b801a688fc51b2756e63ff4
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-20 22:11:12 +0530

    Clean up initial variable declarations in solve method
    Removed unused variable declarations and imports.

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,11 +3,6 @@ class Solution:
     if not board:
       return
 
-    DIRS = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    m = len(board)
-    n = len(board[0])
-    q = collections.deque()
-
   
          
       
