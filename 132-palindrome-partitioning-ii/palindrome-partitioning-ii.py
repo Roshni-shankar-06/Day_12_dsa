50a470b27a9e635bf17058774d8eec9df4d3f5cd
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-20 22:09:25 +0530

    Update palindrome-partitioning-ii.py

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -2,11 +2,6 @@ class Solution:
   def minCut(self, s: str) -> int:
     n = len(s)
     # isPalindrome[i][j] := True if s[i..j] is a palindrome
-    isPalindrome = [[True] * n for _ in range(n)]
-    # dp[i] := the minimum cuts needed for a palindrome partitioning of s[0..i]
-    dp = [n] * n
-
-    for l in range(2, n + 1):
    
       
 
