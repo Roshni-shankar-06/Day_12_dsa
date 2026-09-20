0284ff805545ac0f132513d585f058ee60881dbc
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-20 22:12:14 +0530

    Refactor dfs function in sum-root-to-leaf-numbers.py
    Refactor dfs function to handle leaf nodes and path calculation.

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -5,9 +5,4 @@ class Solution:
     def dfs(root: TreeNode | None, path: int) -> None:
       nonlocal ans
       if not root:
-        return
-      if not root.left and not root.right:
-        ans += path * 10 + root.val
-        return
-
-    
+   
