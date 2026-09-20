466e68850be40d5caf0f003ae81a8e94ab44085c
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-20 22:05:56 +0530

    Refactor reverseDegree function
    Refactor reverseDegree function to improve logic.

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -3,7 +3,4 @@ class Solution {
   int reverseDegree(string s) {
     int ans = 0;
     for (int i = 0; i < s.length(); ++i) {
-      const int reversePos = 26 - (s[i] - 'a');
-      ans += reversePos * (i + 1);
-    }
- 
+    
